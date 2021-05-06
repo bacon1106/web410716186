d7acb5c60624607c9c5e314487738926193e9c18
黃培瑞 <fr5689473@gmail.com>
2021-05-06 20:10:24 +0800

    .

--- Web410716186/Controllers/HomeController.cs
+++ Web410716186/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Web410716186.Controllers
+namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
